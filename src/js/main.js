4b6d92a0ad0e2aaadf56fa9293271620726248ea
Brandon Smith <Brandon@reddirtwebdesign.com>
2017-10-09 03:21:30 -0700

    Moving to GitHub Pages

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+$(function(){
+
+    $(".video").click(function(){
+        var theModal = $(this).data("target"),
+        videoSRC = $(this).attr("data-video"),
+        videoSRCauto = videoSRC + 
+        "?modestbranding=1&rel=0&controls=0&showinfo=0&html15-1&autoplay=1";
+        $(theModal + ' iframe').attr('src', videoSRCauto);
+        $(theModal + ' button.close').click(function(){
+            $(theModal + ' iframe').attr('src', videoSRC);
+        });
+    });
+});
+
+$(document).on('click','[data-toggle="lightbox"]',function(){
+    event.preventDefault();
+    $(this).ekkoLightbox();
+})
